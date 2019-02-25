142a687519de417b0a3af5e8febe6a21c6305888
lzq0220 <47165929+lzq0220@users.noreply.github.com>
2019-02-25 10:50:16 +0800

    Update vsphere.tf

--- vsphere.tf
+++ vsphere.tf
@@ -1,7 +1,7 @@
 provider "vsphere" {
-  user           = "root"
-  password       = "1234567"
-  vsphere_server = "192.168.10.3"
+  user           = "administrator@vsphere.local"
+  password       = "Hnky@1234"
+  vsphere_server = "192.168.10.201"
 
   # If you have a self-signed cert
   allow_unverified_ssl = true
@@ -11,13 +11,23 @@ data "vsphere_datacenter" "dc" {
   name = "DataCenter"
 }
 
+data "vsphere_compute_cluster" "cluster" {
+  name          = "Cluster"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = "Ubuntu1604"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
 data "vsphere_datastore" "datastore" {
   name          = "192.168.10.3-HDD"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 data "vsphere_resource_pool" "pool" {
-  name          = "DataCenter\terraform"
+  name          = "terraform"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
