da77711440e1796a7f8781d643c410851bd600af
lzq0220 <47165929+lzq0220@users.noreply.github.com>
2019-03-08 15:25:07 +0800

    Update vsphere.tf

--- vsphere.tf
+++ vsphere.tf
@@ -1,47 +1,42 @@
-provider "vsphere" {
-    user           = "root"
-  password       = "1234567"
-  vsphere_server = "192.168.10.3"
-
-  # If you have a self-signed cert
-  allow_unverified_ssl = true
-}
-
-data "vsphere_datacenter" "dc" {
-  name = "Datacenter"
-
-}
-
-data "vsphere_datastore" "datastore" {
-  name          = "192.168.10.3-HDD"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
-}
-
-data "vsphere_resource_pool" "pool" {
-  name          = "terraform"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
-}
-
-data "vsphere_network" "network" {
-  name          = "VM Network"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
-}
-
 resource "vsphere_virtual_machine" "vm" {
-  name             = "terraform-0123"
-  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  name             = "terraform-clonetest"
+  resource_pool_id = "${data.vsphere_compute_cluster.cluster.resource_pool_id}"
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
 
-  num_cpus = 2
-  memory   = 1024
-  guest_id = "other3xLinux64Guest"
+  num_cpus = 1 
+  memory   = 2048
+  guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
+
+ # scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
 
   network_interface {
-    network_id = "${data.vsphere_network.network.id}"
+    network_id   = "${data.vsphere_network.network.id}"
+    adapter_type = "${data.vsphere_virtual_machine.template.network_interface_types[0]}"
   }
 
+
   disk {
-    label = "disk0"
-    size  = 20
+    label            = "disk0"
+    size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
+    eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
+    thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
+  }
+
+  clone {
+    template_uuid = "${data.vsphere_virtual_machine.template.id}"
+
+    customize {
+      linux_options {
+        host_name = "terraform-clonetest"
+        domain    = "test.internal"
+      }
+
+      network_interface {
+        ipv4_address = "192.168.10.110"
+        ipv4_netmask = 24
+      }
+
+      ipv4_gateway = "192.168.10.254"
+    }
   }
 }
