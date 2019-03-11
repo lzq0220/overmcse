e8a90e015b65e1b4accd8e691590f8349c03eb32
lzq0220 <47165929+lzq0220@users.noreply.github.com>
2019-03-11 10:34:00 +0800

    Add files via upload

--- vsphere.tf
+++ vsphere.tf
@@ -1,42 +1,27 @@
-resource "vsphere_virtual_machine" "vm" {
-  name             = "terraform-clonetest"
-  resource_pool_id = "${data.vsphere_compute_cluster.cluster.resource_pool_id}"
-  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+provider "vsphere" {
+  user           = "root"
+  password       = "1234567"
+  vsphere_server = "192.168.10.3"
 
-  num_cpus = 2 
-  memory   = 4048
-  guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
 
- # scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
+data "vsphere_datacenter" "dc" {
+  name = "datacenter"
+}
 
-  network_interface {
-    network_id   = "${data.vsphere_network.network.id}"
-    adapter_type = "${data.vsphere_virtual_machine.template.network_interface_types[0]}"
-  }
+data "sphere_datastore" "datastore" {
+  name          = "192.168.10.3-HDD"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
 
+data "vsphere_resource_pool" "pool" {
+  name          = "datacenter\terraform"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
 
-  disk {
-    label            = "disk0"
-    size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
-    eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
-    thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
-  }
-
-  clone {
-    template_uuid = "${data.vsphere_virtual_machine.template.id}"
-
-    customize {
-      linux_options {
-        host_name = "terraform-clonetest"
-        domain    = "test.internal"
-      }
-
-      network_interface {
-        ipv4_address = "192.168.10.111"
-        ipv4_netmask = 24
-      }
-
-      ipv4_gateway = "192.168.10.254"
-    }
-  }
+data "vsphere_network" "network" {
+  name          = "VM Network"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
